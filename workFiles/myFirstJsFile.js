542d2b58bb9a399e20dbc77a19b2588afa05865f
nikizhelqzkov <nikizhelqzkov@gmail.com>
2020-10-06 01:12:35 +0300

    new material for js

---
+++ workFiles/myFirstJsFile.js
@@ -0,0 +1,11 @@
+let a = 'Hello';
+//alert("hello");
+console.log(a);
+
+let isItOk = false;
+isItOk = true;
+document.write(isItOk);
+let digitNumbers = [1,2,3,4,5,isItOk,a,"haha"];
+console.log(digitNumbers[3]);
+console.log(typeof(96));
+

b22b54cd07e403eb0b443b8e53c9ebf6da2373ab
Your Name <nikolajzz@uni-sofia.bg>
2020-10-10 15:26:33 +0300

    adding materials

---
+++ workFiles/js_operators.js
@@ -0,0 +1,35 @@
+let n = 5%2;//1
+let sum = 6;
+sum *= n;//6*1
+//console.log(sum);
+//sum+=n   sum = sum + n  
+let firstName = "Nikolay";
+let lastName = "Zhelyazkov";
+let name = firstName + ' ' +  lastName;
+name+= firstName;
+console.log(name);
+let isTrue = (sum === 6);
+//console.log(isTrue);
+let isT = (n!=='1');
+//console.log(isT);
+let num = 15%3;//0
+let less = (n>=num);
+let even = (5%2===0);
+//console.log(less);
+//console.log("hello"!=="hello");
+let r = 3;
+//r+=1 r = r + 1 r++
+
+// let arr = [3,5,28,963,58,4,56,2];
+// let avr = (arr[3] + arr[4])/2;
+// let s2 = arr[5] + arr[6] + arr[7];
+//console.log((arr[0] + arr[7])===arr[1]);
+// console.log(avr>s2);
+//iskame : sredno aritmetichno(963,58) dali e po-golqmo ot sum(4 56 2)
+
+//iskame da proverim vseki element ot masiva dali e cheten
+let a = [3,68,22,12];
+console.log((a[0]%2)===0);
+console.log((a[1]%2)===0);
+console.log((a[2]%2)===0);
+console.log((a[3]%2)===0);

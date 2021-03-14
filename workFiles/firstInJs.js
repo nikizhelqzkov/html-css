c2b53905b196ae3cfe21818dd96a4d456aa9e32e
nikizhelqzkov <nikizhelqzkov@gmail.com>
2021-03-14 17:56:41 +0200

    update

---
+++ workFiles/firstInJs.js
@@ -0,0 +1,59 @@
+let age = 0;
+console.log(Math.round(age / 3));
+console.log(age % 10); //ТОВА НЕЩО НИ ПОКАЗВА КОЛКО ЩЕ БЪДЕ ОСТАТАКЪТ!
+let name = "20";
+let name2 = 130;
+let isAdult = age > 18;
+console.log(isAdult);
+isAdult = false;
+console.log(isAdult);
+let masiv = [1,2,3,45,56,8,2,56,8,2,"jdjkf",age,90];
+let word = ['n','i','g','g','e','r'];
+console.log(masiv.length);
+console.log(masiv[masiv.length-2]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+''
+
+let age1 = 3
+isPulnoleten = 18>age1

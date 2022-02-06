a869dcb3d0193bc2a87c9e95b80b4d0e443cec97
nikizhelqzkov <nikizhelqzkov@gmail.com>
2022-02-06 15:17:15 +0200

    new exercises

---
+++ workFiles/fExJs.js
@@ -0,0 +1,23 @@
+console.log("Hello from console", 23);
+// alert("Hello from alert");
+var message = "Hello var";
+console.log(message);
+
+{
+let name = "NIki";
+// console.log(name);
+console.log(name);
+let arr = [1,12,5,"hey"];
+let arr2 = arr;
+let a = 1;
+const ar = [1,12,5,"ewrrf"];
+console.log(a===1);
+console.log(typeof a);
+let isOk = true;
+
+let s = ar.join('64513')
+let ss = 123566;
+let revS = +ss.toString().split('').reverse().join('');
+console.log(revS);
+}
+ 
